c9fd78793914c05bc4a5d3d1f04dd9b20adc3bbf
Charles Francoise <charles.francoise@circleci.com>
2024-03-18 19:58:19 +0100

    show vercel URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default async function Home() {
   const res = await fetch(`https://${process.env.VERCEL_URL}/api/message`);
   const message = await res.text();
-  return <main>{message}</main>;
+  return (
+    <main>
+      <h6>{process.env.VERCEL_URL}</h6>
+    </main>
+  );
 }
