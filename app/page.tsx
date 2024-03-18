dd8b850742f7c891ae605e5e2b3cba36133312fb
Charles Francoise <charles.francoise@circleci.com>
2024-03-18 19:32:16 +0100

    fix API url

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/message`);
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/message`);
   const message = await res.text();
   return <main>{message}</main>;
 }
