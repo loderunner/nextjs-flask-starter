e9fad58ac4f1877b1ff54ff171105a5b5784ca2c
Charles Francoise <charles.francoise@circleci.com>
2024-03-18 20:16:01 +0100

    use env var for api url

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 export default async function Home() {
-  const res = await fetch(`https://${process.env.VERCEL_URL}/api/message`);
+  const res = await fetch(`${process.env.API_URL}/message`);
   const message = await res.text();
-  return (
-    <main>
-      <h6>{process.env.VERCEL_URL}</h6>
-    </main>
-  );
+  return <main>{message}</main>;
 }
